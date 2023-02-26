ef171d2f1f810b6ba1b8627d630abf95c937e5a3
Aithus <aithus@163.com>
2023-02-26 22:13:05 +0800

    finish lm

---
+++ model/__init__.py
@@ -0,0 +1,9 @@
+from .encoder import Encoder
+from .decoder import Decoder
+from .lm import LM
+
+__all__ = [
+    "Encoder", 
+    "Decoder", 
+    "LM"
+]
---
+++ model/lm.py
@@ -0,0 +1,80 @@
+from copy import deepcopy
+import numpy as np
+
+import torch
+from torch import nn
+import torch.nn.functional as F
+
+from encoder import Encoder
+from decoder import Decoder
+
+
+class LM(nn.Module):
+    """Language model that predict next word based on previous N-1 gram"""
+    def __init__(
+        self, 
+        N, # Ngram
+        v_size, 
+        embedding_size, 
+        # hidden layers used to predict the embedding of next 
+        # word by previous embedding, also used in encoder and 
+        # decoder
+        hidden_sizes=[32], 
+        activation=nn.ReLU,
+        device='cuda'
+    ):
+        super().__init__()
+
+        assert N == 2, 'only support bigram now!'
+
+        self.N = N
+        self.v_size = v_size
+        self.embedding_size = embedding_size
+        self.hidden_sizes = hidden_sizes
+        self.layers = [self.embedding_size] + hidden_sizes + [self.embedding_size]
+        self.device = device
+
+        m = [
+            [nn.Linear(i, j), activation()] for i, j in
+            zip(self.layers[:-1], self.layers[1:])
+        ]
+        # from https://stackoverflow.com/questions/952914/how-do-i-make-a-flat-list-out-of-a-list-of-lists
+        m = [i for l in m for i in l]
+
+        self.model = nn.Sequential(*m[:-1])
+        self.model = self.model.to(self.device)
+
+        self.encoder = Encoder(N, v_size, 
+                               embedding_size, 
+                               hidden_sizes, 
+                               activation, 
+                               device=device)
+        self.decoder = Decoder(v_size, 
+                               embedding_size, 
+                               hidden_sizes, 
+                               activation, 
+                               device=device)
+
+
+    def forward(self, x):
+        '''Map: tensor(B, N) -> tensor(B, N, E)'''
+        # one hot encoding
+        import ipdb; ipdb.set_trace()
+        x = x.to(self.device)
+
+        x = self.encoder(x)
+        # use mean pooling
+        x = x.mean(1)
+        x = self.model(x)
+        x = self.decoder(x)
+        return x
+
+
+
+if __name__ == '__main__':
+    x = torch.tensor([
+        [1], 
+        [0], 
+        [0]])
+    lm = LM(2, 2, 2)
+    print(lm(x).shape)
